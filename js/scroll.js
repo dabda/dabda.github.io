2d2b3bc39f39eed6631963c5f02444dd19f43dec
dabda <damjan.vidonja@gmail.com>
2017-08-22 16:33:02 +0200

    menu precrta besede

--- js/scroll.js
+++ js/scroll.js
@@ -27,12 +27,51 @@ $(document).ready(function(){
 $(document).ready(function() {
     $(window).scroll(function() {
       var h = (document.documentElement.scrollTop || document.body.scrollTop);
-      var h2 = $(window).scrollBottom();
+      //var h2 = $(window).scrollBottom();
       var visina = $(document).height();
-      console.log(h);
-      console.log(h2);
-      console.log();
-      document.getElementById("visina").setAttribute("startOffset", (h/visina)*5000);
+      var razmerje = (h/visina)*5000;
+      if ( razmerje > 4100) {
+        document.getElementById("visina").setAttribute("startOffset", 4100);
+      }
+      else{
+        document.getElementById("visina").setAttribute("startOffset", (h/visina)*5000);
+      }
+      if (razmerje >= 0 && razmerje < 800) {
+        $("#menu_about").addClass("precrtaj");
+        $("#menu_breweries").removeClass("precrtaj");
+        $("#menu_music").removeClass("precrtaj");
+        $("#menu_food").removeClass("precrtaj");
+        $("#menu_timeline").removeClass("precrtaj");
+      }
+      else if (razmerje >= 800 && razmerje < 1600) {
+        $("#menu_about").removeClass("precrtaj");
+        $("#menu_breweries").addClass("precrtaj");
+        $("#menu_music").removeClass("precrtaj");
+        $("#menu_food").removeClass("precrtaj");
+        $("#menu_timeline").removeClass("precrtaj");
+      }
+      else if (razmerje >= 1600 && razmerje < 2400) {
+        $("#menu_about").removeClass("precrtaj");
+        $("#menu_breweries").removeClass("precrtaj");
+        $("#menu_music").addClass("precrtaj");
+        $("#menu_food").removeClass("precrtaj");
+        $("#menu_timeline").removeClass("precrtaj");
+      }
+      else if (razmerje >= 2400 && razmerje < 3200) {
+        $("#menu_about").removeClass("precrtaj");
+        $("#menu_breweries").removeClass("precrtaj");
+        $("#menu_music").removeClass("precrtaj");
+        $("#menu_food").addClass("precrtaj");
+        $("#menu_timeline").removeClass("precrtaj");
+      }
+      else if (razmerje >= 3200 && razmerje < 4100) {
+        $("#menu_about").removeClass("precrtaj");
+        $("#menu_breweries").removeClass("precrtaj");
+        $("#menu_music").removeClass("precrtaj");
+        $("#menu_food").removeClass("precrtaj");
+        $("#menu_timeline").addClass("precrtaj");
+      }
+
     });
 });
 //var number=document.getElementById("visina").getAttribute('startOffset'); 
