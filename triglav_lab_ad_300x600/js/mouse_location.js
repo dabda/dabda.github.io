65351d8bd4281f98979e0ac01b4682bd47d30273
dabda <damjan.vidonja@gmail.com>
2018-03-04 22:03:31 +0100

    300x600 parent

--- triglav_lab_ad_300x600/js/mouse_location.js
+++ triglav_lab_ad_300x600/js/mouse_location.js
@@ -2,12 +2,13 @@ var className;
 var numberOfPics=24;
 
 function HeadImage(className){
-    
+
     /* Setting the global instance of classname to the given parameter*/
     this.className = className;
-    
+
     /* Calculating the borders and step of the image */
-    var imageWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    var imageWidth = parent.document.body.clientWidth
+    //var imageWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
     step = imageWidth / numberOfPics;
 
     /* This function determines where the mouse pointer is relative to the image
