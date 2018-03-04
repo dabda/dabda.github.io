6a054fa43201bbc3dfafa9a33fafaf1bc53075c4
dabda <damjan.vidonja@gmail.com>
2018-03-04 21:19:32 +0100

    triglav

---
+++ triglav_lab_ad_300x250/js/mouse_location.js
@@ -0,0 +1,24 @@
+var className;
+var numberOfPics=25;
+
+function HeadImage(className){
+    
+    /* Setting the global instance of classname to the given parameter*/
+    this.className = className;
+    
+    /* Calculating the borders and step of the image */
+    var imageWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    step = imageWidth / numberOfPics;
+
+    /* This function determines where the mouse pointer is relative to the image
+     * and displays the correct image accordingly. */
+    this.setImageDirection = function(){
+        slika = Math.round(mouseX/step) + 1;
+        for (const element of document.querySelectorAll("."+this.className+" img")) {
+            element.style.zIndex = 0;
+            if (element.classList.contains(slika.toString())){
+                element.style.zIndex = 1;
+            }
+        }
+    };
+}
