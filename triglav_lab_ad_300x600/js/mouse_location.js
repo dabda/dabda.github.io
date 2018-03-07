1f02cf5875f18d1b529def7ae8ffb9bff1c331be
dabda <damjan.vidonja@gmail.com>
2018-03-07 23:55:51 +0100

    mouse inside test

--- triglav_lab_ad_300x600/js/mouse_location.js
+++ triglav_lab_ad_300x600/js/mouse_location.js
@@ -23,3 +23,28 @@ function HeadImage(className){
         }
     };
 }
+
+function HeadImage_inside(className){
+
+    /* Setting the global instance of classname to the given parameter*/
+    this.className = className;
+
+    /* Calculating the borders and step of the image */
+    var imageWidth = parent.document.body.clientWidth;
+    // Get Left Position
+    var iframeX = window.parent.document.getElementById('triglav_lab_300x600').offsetLeft;
+    //var imageWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    step = imageWidth / numberOfPics;
+
+    /* This function determines where the mouse pointer is relative to the image
+     * and displays the correct image accordingly. */
+    this.setImageDirection = function(){
+        slika = Math.round((mouseX300x600_inside+iframeX)/step) + 1;
+        for (const element of document.querySelectorAll("."+this.className+" img")) {
+            element.style.zIndex = 0;
+            if (element.classList.contains(slika.toString())){
+                element.style.zIndex = 1;
+            }
+        }
+    };
+}
